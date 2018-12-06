a0939a5359cfe6fdeeec8c8fe0f75160743746f0
Marcos Aguayo <marcosaguayomora@gmail.com>
2018-12-06 01:28:23 +0000

    Updating travis (#4)
    * Updating travis
    
    * Update .travis.yml

--- coverage.sh
+++ coverage.sh
@@ -3,4 +3,4 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 
 coverage run --source=luracoin -- ${PYTEST} -v ${ROOT_DIR} ${@} --ignore=venv/
 coverage html -d artifacts
-coverage report -m --fail-under=88
+coverage report -m --fail-under=90
