35f4d1e80ace6442b2b76e321ead260c6cb3b834
Marcos Aguayo <marcos@aguayo.es>
2018-12-18 01:26:44 +0000

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 PYTEST=$(which pytest)
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-coverage run --source=luracoin -- ${PYTEST} -v ${ROOT_DIR} ${@} --ignore=venv/
+coverage run --omit *client.py --source=luracoin -- ${PYTEST} -v ${ROOT_DIR} ${@}
 coverage html -d artifacts
 coverage report -m --fail-under=90
