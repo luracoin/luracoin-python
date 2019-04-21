43ab28899386701f60ecad7fcbfbb44225a3ff2a
Marcos Aguayo <marcosaguayomora@gmail.com>
2019-04-21 22:05:18 +0100

    Starting with LevelDB and blk files (#14)
    * Added functions get_current_file_number and get_current_file_name
    
    * upgrade plyvel
    
    * blockchain fixture
    
    * mypy flake8 and black
    
    * linting

---
+++ luracoin/chain.py
@@ -0,0 +1,22 @@
+import plyvel
+from luracoin.config import Config
+
+
+def get_current_file_number() -> str:
+    # Get the current file
+    db = plyvel.DB(Config.BLOCKS_DIR + "index", create_if_missing=True)
+    file_number = db.get(b"l")
+
+    # If there is not a current file we'll start by '000000'
+    if file_number is None or file_number == "" or file_number == b"":
+        file_number = "000000"
+    else:
+        file_number.decode("utf-8")
+
+    db.close()
+
+    return file_number
+
+
+def get_current_file_name(file_number: str) -> str:
+    return f"blk{file_number}.dat"
