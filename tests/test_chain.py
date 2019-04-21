7bd8aeaa58e87d8dae196edd3b2eb0a782821fe6
Marcos Aguayo <marcos@aguayo.es>
2019-04-21 17:40:42 +0100

    Added functions get_current_file_number and get_current_file_name

---
+++ tests/test_chain.py
@@ -0,0 +1,13 @@
+import pytest
+
+from luracoin.chain import get_current_file_number, get_current_file_name
+
+
+def test_get_current_file_number():
+    assert get_current_file_number() == "000000"
+
+
+def test_get_current_file_name():
+    assert get_current_file_name("000000") == "blk000000.dat"
+    assert get_current_file_name("000001") == "blk000001.dat"
+    assert get_current_file_name("021056") == "blk021056.dat"
