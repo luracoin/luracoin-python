ced94bc88538871fd083af3d531b63a7dbd8216a
Marcos Aguayo <marcosaguayomora@gmail.com>
2021-11-19 08:29:21 +0100

    make sure addresses are 34 chars long

--- luracoin/wallet.py
+++ luracoin/wallet.py
@@ -96,4 +96,7 @@ def pubkey_to_address(pubkey: bytes) -> str:
 
     sha = hashlib.sha256(pubkey).digest()
     ripe = hashlib.new("ripemd160", sha).digest()
-    return b58encode_check(b"\x00" + ripe)
+    address = b58encode_check(b"\x00" + ripe)
+    if len(address) != 34:
+        raise RuntimeError("Invalid address")
+    return address
